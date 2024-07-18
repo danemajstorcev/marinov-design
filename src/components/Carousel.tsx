8629ba7e597e25c67e5daf4bd253757f51fbed92
Dane.Majstorcev-FE12 <majstorcev.dane@gmail.com>
2024-07-18 09:49:37 +0200

    changed carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,63 +1,4 @@
-// import React, { useState } from 'react';
-
-// // ... (your imports)
-
-// interface CarouselProps {
-//     images: string[];
-//   }
-  
-//   const Carousel: React.FC<CarouselProps> = ({ images }) => {
-//     const [currentIndex, setCurrentIndex] = useState(0);
-  
-//     const nextSlide = () => {
-//       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-//     };
-  
-//     const prevSlide = () => {
-//       setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-//     };
-  
-//     const goToSlide = (index: number) => {
-//       setCurrentIndex(index);
-//     };
-  
-//     return (
-//       <div className="carousel">
-//         <div className="slides-container">
-//           {images.map((image, index) => (
-//             <img
-//               key={index}
-//               className={`slide ${index === currentIndex ? 'active' : ''}`}
-//               src={image}
-//               alt={`Slide ${index + 1}`}
-//             />
-//           ))}
-//         </div>
-  
-//         <div className="arrows">
-//           <button className="arrow prev" onClick={prevSlide}>
-//             &lt;
-//           </button>
-//           <button className="arrow next" onClick={nextSlide}>
-//             &gt;
-//           </button>
-//         </div>
-  
-//         <div className="dots">
-//           {images.map((_, index) => (
-//             <span
-//               key={index}
-//               className={`dot ${index === currentIndex ? 'active' : ''}`}
-//               onClick={() => goToSlide(index)}
-//             />
-//           ))}
-//         </div>
-//       </div>
-//     );
-//   };
-  
-//   export default Carousel;
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 interface CarouselProps {
   images: string[];
@@ -71,7 +12,9 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
   };
 
   const prevSlide = () => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length
+    );
   };
 
   const goToSlide = (index: number) => {
@@ -80,7 +23,10 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
 
   return (
     <div className="carousel">
-      <div className="slides-container" style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+      <div
+        className="slides-container"
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+      >
         {images.map((image, index) => (
           <img
             key={index}
@@ -92,22 +38,26 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
       </div>
 
       <div className="arrows">
-        <img className='arrow prev' onClick={prevSlide} src="/images/our_story_page/arrow-left.png" alt="" />
-        {/* <button className="arrow prev" onClick={prevSlide}>
-          &lt;
-        </button> */}
-                <img className='arrow next' onClick={nextSlide} src="/images/our_story_page/arrow-right.png" alt="" />
+        <img
+          className="arrow prev"
+          onClick={prevSlide}
+          src="/images/our_story_page/arrow-left.png"
+          alt="Previous slide"
+        />
 
-        {/* <button className="arrow next" onClick={nextSlide}>
-          &gt;
-        </button> */}
+        <img
+          className="arrow next"
+          onClick={nextSlide}
+          src="/images/our_story_page/arrow-right.png"
+          alt="Next slide"
+        />
       </div>
 
       <div className="dots">
         {images.map((_, index) => (
           <span
             key={index}
-            className={`dot ${index === currentIndex ? 'active' : ''}`}
+            className={`dot ${index === currentIndex ? "active" : ""}`}
             onClick={() => goToSlide(index)}
           />
         ))}
