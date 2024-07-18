b6f76d0426710ce47d178d6f00cb8fbc44c5552c
Dane.Majstorcev-FE12 <majstorcev.dane@gmail.com>
2024-07-18 12:11:21 +0200

    next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true, // Enable SWC compiler for faster builds (optional)
+  images: {
+    domains: ['example.com'], // Add domains for external images
+  },
+  // You can add other configurations here
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
